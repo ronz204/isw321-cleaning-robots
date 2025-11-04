57339afce8c5f1bf5725cdf8c2687d7b038da668
roney ruiz <roneyruizrojas@gmail.com>
2025-11-03 22:28:31 -0600

    feat: add save room method to room repository

--- src/com/isw/app/repositories/RoomRepository.java
+++ src/com/isw/app/repositories/RoomRepository.java
@@ -2,6 +2,9 @@ package com.isw.app.repositories;
 
 import java.io.IOException;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import com.isw.app.models.Room;
+import com.isw.app.models.Sector;
 import com.isw.app.enums.DataFile;
 
 public class RoomRepository extends BaseRepository {
@@ -15,6 +18,36 @@ public class RoomRepository extends BaseRepository {
       while ((line = reader.readLine()) != null) {
         System.out.println(line);
       }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void saveRoom(Room room) {
+    try (BufferedWriter writer = this.getAppendWriter()) {
+      writer.write("$");
+      writer.newLine();
+      
+      writer.write(room.getUuid());
+      writer.newLine();
+      
+      Sector[][] sectors = room.getSectors();
+      for (int row = 0; row < room.getRows(); row++) {
+        StringBuilder builder = new StringBuilder();
+
+        for (int col = 0; col < room.getCols(); col++) {
+          builder.append(sectors[row][col].getType().getPrefix());
+          if (col < room.getCols() - 1) {
+            builder.append(" ");
+          }
+        }
+
+        writer.write(builder.toString());
+        writer.newLine();
+      }
+      
+      writer.write("$");
+      writer.newLine();
     } catch (IOException e) {
       e.printStackTrace();
     }
