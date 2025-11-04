3131c2744e1db6a77804f89e2aec0c57b41a9151
roney ruiz <roneyruizrojas@gmail.com>
2025-11-03 21:37:44 -0600

    feat: add weights to random getter of sector type

--- src/com/isw/app/enums/SectorType.java
+++ src/com/isw/app/enums/SectorType.java
@@ -3,30 +3,43 @@ package com.isw.app.enums;
 import com.isw.app.helpers.RandomHelper;
 
 public enum SectorType {
-  DIRTY("Sucia"),
-  CLEAN("Limpia"),
-  RECHARGE("Recarga"),
-  TEMPORARY("Temporal"),
-  OBSTRUCTED("Obstruida");
+  DIRTY("Sucia", 30),
+  CLEAN("Limpia", 50),
+  RECHARGE("Recarga", 10),
+  TEMPORARY("Temporal", 10),
+  OBSTRUCTED("Obstruida", 10);
 
   private final String label;
   private final char prefix;
+  private final int weight;
 
-  SectorType(String label) {
+  SectorType(String label, int weight) {
     this.label = label;
+    this.weight = weight;
     this.prefix = label.charAt(0);
   }
 
-  public String getLabel() {
-    return label;
+  public int getWeight() {
+    return weight;
   }
 
   public char getPrefix() {
     return prefix;
   }
 
+  public String getLabel() {
+    return label;
+  }
+
   public static SectorType getRandomType() {
-    SectorType[] values = SectorType.values();
-    return values[RandomHelper.getRandomInt(0, values.length - 1)];
+    int value = RandomHelper.getRandomInt(1, 100);
+    int cumulative = 0;
+
+    for (SectorType type : SectorType.values()) {
+      cumulative += type.getWeight();
+      if (value <= cumulative) return type;
+    }
+
+    return CLEAN;
   }
 }
