9b09ef15fde1aa1e3bfdee93011905c707f15a19
roney ruiz <roneyruizrojas@gmail.com>
2025-11-04 20:33:18 -0600

    feat: add colors to sector types

--- src/com/isw/app/enums/SectorType.java
+++ src/com/isw/app/enums/SectorType.java
@@ -1,20 +1,23 @@
 package com.isw.app.enums;
 
+import java.awt.Color;
 import com.isw.app.helpers.RandomHelper;
 
 public enum SectorType {
-  DIRTY("Sucia", 30),
-  CLEAN("Limpia", 50),
-  RECHARGE("Recarga", 10),
-  TEMPORARY("Temporal", 10),
-  OBSTRUCTED("Obstruida", 10);
+  DIRTY("Sucia", 30, Color.ORANGE),
+  CLEAN("Limpia", 50, Color.WHITE),
+  RECHARGE("Recarga", 10, Color.GREEN),
+  TEMPORARY("Temporal", 10, Color.GRAY),
+  OBSTRUCTED("Obstruida", 10, Color.BLACK);
 
   private final String label;
   private final char prefix;
+  private final Color color;
   private final int weight;
 
-  SectorType(String label, int weight) {
+  SectorType(String label, int weight, Color color) {
     this.label = label;
+    this.color = color;
     this.weight = weight;
     this.prefix = label.charAt(0);
   }
@@ -25,6 +28,10 @@ public enum SectorType {
 
   public char getPrefix() {
     return prefix;
+  }
+
+  public Color getColor() {
+    return color;
   }
 
   public String getLabel() {
