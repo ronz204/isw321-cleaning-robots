1509a05a04e6da7e75e0ee527c4c30a71f2b7e4e
roney ruiz <roneyruizrojas@gmail.com>
2025-11-04 20:16:43 -0600

    chore: adjust view imports

--- src/com/isw/app/Main.java
+++ src/com/isw/app/Main.java
@@ -1,6 +1,6 @@
 package com.isw.app;
 
-import com.isw.app.views.SimulatorView;
+import com.isw.app.presentation.views.SimulatorView;
 
 public class Main {
   public static void main(String[] args) {
