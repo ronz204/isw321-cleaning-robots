50d10c3b4bdcab8c5c10cf48e67f0d0b08c20389
roney ruiz <roneyruizrojas@gmail.com>
2025-11-05 16:29:55 -0600

    feat: create room service

---
+++ src/com/isw/app/services/RoomService.java
@@ -0,0 +1,10 @@
+package com.isw.app.services;
+
+import com.isw.app.models.Room;
+
+public class RoomService {
+  public static Room generate() {
+    Room room = new Room();
+    return room;
+  }
+}
