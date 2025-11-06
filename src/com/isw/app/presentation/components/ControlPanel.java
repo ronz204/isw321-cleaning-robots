c8c5e0d5a133e25062c027a46f1baff262e061c7
roney ruiz <roneyruizrojas@gmail.com>
2025-11-05 22:02:32 -0600

    feat: add leyenda for sector colors

--- src/com/isw/app/presentation/components/ControlPanel.java
+++ src/com/isw/app/presentation/components/ControlPanel.java
@@ -9,8 +9,11 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import java.awt.GridLayout;
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+import com.isw.app.enums.SectorType;
 
 public class ControlPanel extends JPanel {
 
@@ -20,6 +23,7 @@ public class ControlPanel extends JPanel {
   private JButton simulateButton;
   private JButton generateButton;
   private JPanel buttonPanel;
+  private JPanel legendPanel;
 
   private Runnable onGenerateCall;
 
@@ -31,6 +35,7 @@ public class ControlPanel extends JPanel {
     buildPlaceButton();
     buildSimulateButton();
     buildExitButton();
+    buildLegendPanel();
   }
 
   private void buildContainer() {
@@ -80,6 +85,46 @@ public class ControlPanel extends JPanel {
     exitButton.addActionListener(this::onExit);
     exitButton.setPreferredSize(new Dimension(140, 30));
     buttonPanel.add(exitButton);
+  }
+
+  private void buildLegendPanel() {
+    legendPanel = new JPanel();
+    legendPanel.setBackground(Color.LIGHT_GRAY);
+    legendPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+    legendPanel.setLayout(new GridLayout(0, 1, 0, 5));
+
+    buildLegendTitle();
+    buildLegendItems();
+
+    this.add(legendPanel, BorderLayout.SOUTH);
+  }
+
+  private void buildLegendTitle() {
+    JLabel legendTitle = new JLabel("Leyenda de Sectores");
+    legendTitle.setHorizontalAlignment(SwingConstants.CENTER);
+    legendTitle.setFont(new Font("Arial", Font.BOLD, 12));
+    legendTitle.setForeground(Color.DARK_GRAY);
+    legendPanel.add(legendTitle);
+  }
+
+  private void buildLegendItems() {
+    for (SectorType type : SectorType.values()) {
+      JPanel itemPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 5, 0));
+      itemPanel.setBackground(Color.LIGHT_GRAY);
+
+      JPanel colorIndicator = new JPanel();
+      colorIndicator.setBackground(type.getColor());
+      colorIndicator.setPreferredSize(new Dimension(15, 15));
+      colorIndicator.setBorder(javax.swing.BorderFactory.createLineBorder(Color.DARK_GRAY, 1));
+
+      JLabel typeLabel = new JLabel(type.getLabel());
+      typeLabel.setFont(new Font("Arial", Font.PLAIN, 10));
+      typeLabel.setForeground(Color.DARK_GRAY);
+
+      itemPanel.add(colorIndicator);
+      itemPanel.add(typeLabel);
+      legendPanel.add(itemPanel);
+    }
   }
 
   public void setOnGenerate(Runnable callback) {
