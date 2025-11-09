9c3b7757bbb5d4fb9530a7403acee2ac8100fe17
roney ruiz <roneyruizrojas@gmail.com>
2025-11-09 10:39:16 -0600

    feat: add recharge counter sector limit

--- src/com/isw/app/models/Room.java
+++ src/com/isw/app/models/Room.java
@@ -60,9 +60,17 @@ public class Room {
   }
 
   private void setupSectorBoard() {
+    final int MAX_RECHARGE = 4;
+
     for (int row = 0; row < ROWS; row++) {
       for (int col = 0; col < COLS; col++) {
         SectorType type = SectorType.getRandomType();
+
+        // Si se genera RECHARGE pero ya hay 4, cambiar a CLEAN
+        if (type == SectorType.RECHARGE && counter.get(SectorType.RECHARGE) >= MAX_RECHARGE) {
+          type = SectorType.CLEAN;
+        }
+
         sectors[row][col] = new Sector(new Coord(row, col), type);
         counter.put(type, counter.get(type) + 1);
       }
